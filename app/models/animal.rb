671f1ce82b74b75375a9c5f98f9dab9074ba2f99
victor dupont <victordupont1407@gmail.com>
2024-06-03 16:55:57 +0200

    validates

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,5 @@
 class Animal < ApplicationRecord
   belongs_to :user
+  has_many :health_records
+  validates :name, :specie, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_03_140326) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_03_143619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_03_140326) do
     t.bigint "animal_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reason"
     t.index ["animal_id"], name: "index_bookings_on_animal_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
