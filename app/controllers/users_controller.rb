c02aadfb3c570e518b1d94510a8a2154d4daf0be
Prunelle Leclerc <prunellelclrc@gmail.com>
2024-06-04 18:03:48 +0200

    debut maps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def index
     @professionals = User.where.not(profession: nil)
     #.order(:profession)
+    @markers = @professionals.geocoded.map do |profesional|
+      {
+        lat: profesional.latitude,
+        lng: profesional.longitude
+      }
+    end
   end
 
   def show
