96cbc9e46ecf852b0f1812809c2a7694e49b5ef7
victor dupont <victordupont1407@gmail.com>
2024-06-04 12:15:34 +0200

    animalcontroller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,39 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: [:update, :destroy]
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to @animal, notice: 'Animal was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @animal.update(animal_params)
+      redirect_to @animal, notice: 'Animal was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @animal.destroy
+    redirect_to animals_url, notice: 'Animal was successfully destroyed.'
+  end
+
+  private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :birthdate, :specie, :breed, :user_id)
+  end
 end
