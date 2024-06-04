591334554aff6a3476f4fdc35348fc505b7a1c73
victor dupont <victordupont1407@gmail.com>
2024-06-04 15:25:28 +0200

    profile

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -4,4 +4,11 @@ class Animal < ApplicationRecord
   belongs_to :user
   has_many :health_records
   validates :name, :specie, presence: true
+
+  def age
+    return unless birthdate
+
+    now = Time.zone.now.to_date
+    now.year - birthdate.year - ((now.month > birthdate.month || (now.month == birthdate.month && now.day >= birthdate.day)) ? 0 : 1)
+  end
 end
