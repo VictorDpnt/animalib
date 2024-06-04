cd8904fed9440cfc8afbd00a1233e77806613bb6
victor dupont <victordupont1407@gmail.com>
2024-06-04 17:47:27 +0200

    profile booking

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-User.destroy_all
+
+Booking.destroy_all
 Animal.destroy_all
+User.destroy_all
 
 user1 = User.create!(email: "test@test.com", password: "azerty", first_name: "Victor", last_name: "Dupont", address: "16 villa gaudelet", phone_number: "0628073198")
 user2 = User.create!(email: "pro@pro.com", password: "azerty", first_name: "Prunelle", last_name: "Leclerc", address: "16 avenue Jean Jaurès", phone_number: "0675654776", profession: "veterinarian", company_name: "Cabinet Vétérinaire de Paris")
 
-Animal.create!(name: "Happy", birthdate: "2024-03-01", specie: "dog", breed: "golden", user: user1)
+animal1 = Animal.create!(name: "Happy", birthdate: "2024-03-01", specie: "dog", breed: "golden", user: user1)
+
+Booking.create!(date: "2024-07-01", user: user2, animal: animal1, reason: "Castration des coucouilles")
+Booking.create!(date: "2024-07-01", user: user2, animal: animal1, reason: "Problème gastrique")
