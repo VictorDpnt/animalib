9848d8f3f43db3977835ba34fe017fea56da369b
victor dupont <victordupont1407@gmail.com>
2024-06-10 18:09:24 +0200

    fixedModal

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  config.cache_classes = true
   # Do not eager load code on boot.
   config.eager_load = false
 
