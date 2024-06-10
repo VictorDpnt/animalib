9b2c2ec3c2a7216235786c401901a53e1b5cade9
Prunelle Leclerc <prunellelclrc@gmail.com>
2024-06-10 14:42:51 +0200

    fav

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  def index
+    @favorites = current_user.favorites
+  end
+
+  def create
+    @user = User.find(params[:user_id]) # c'est le pro
+    @favorite = Favorite.new(professional: @user)
+    @favorite.user = current_user
+    if @favorite.save
+      redirect_to user_path(@user)
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @favorite = Favorite.find_by(professional: @user, user: current_user)
+    @favorite.destroy if @favorite
+
+    redirect_to user_path(@user), notice: "Favorite removed successfully"
+  end
+end
