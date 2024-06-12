d3f33c275e1631e19e8d211e3bb134ac2fc755a1
victor dupont <victordupont1407@gmail.com>
2024-06-12 13:05:45 +0200

    calendar

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,19 @@ class Booking < ApplicationRecord
   validates :date, presence: true
 
   REASONS = ["Follow-up consultation", "Vaccination", "Sterilization", "Digestive diseases", "Trauma", "Other"]
+
+
+  def start_time
+    return self.date
+  end
+
+  def end_time
+    return unless start_time.present?
+
+    start_time + 30.minutes
+  end
+
+  def time
+  "#{start_time.strftime('%H:%M')} - #{end_time.strftime('%H:%M')}"
+  end
 end
